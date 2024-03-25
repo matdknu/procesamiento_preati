4f9121409ad0893ad1aff67699b6d716a0806999
Matías Deneken <m.deneken@uc.cl>
2024-03-25 17:20:06 -0300

    procesamiento variables

--- data_processing/process_data.r
+++ data_processing/process_data.r
@@ -24,10 +24,34 @@ bbdd <- bbdd %>% mutate_all(funs(ifelse(is.na(.), "No responde", .)))
 
 ### Cambio de nombre de variables ----
 
+# Renombrar todas las variables
+names(bbdd) <- c("genero", "edad", "pueblo_indigena", "educacion", "conexion_internet",
+                 "comuna_emprendimiento", "comuna_emprendimiento_otra", "zona_emprendimiento",
+                 "sociedad_emprendimiento", "sociedad_emprendimiento_otra", "año_inicio",
+                 "turismo_principal", "temporada_abierto", "tramite_formalizacion",
+                 "inicio_actividades", "tipos_sociedad", "tipos_sociedad_otras",
+                 "categoria_sernatur", "justificacion_no_inicio", "sello_indigena",
+                 "espacio_desarrollo", "instalaciones_turisticas", "instalaciones_turisticas_otro",
+                 "herramienta_difusion", "herramienta_difusion_otra", "idioma", "idioma_otra",
+                 "organizaciones_indigenas", "organizaciones_no_indigenas", "organizaciones_no_indigenas_cual",
+                 "servicios_turisticos", "servicios_turisticos_otro", "servicios_recreativos",
+                 "servicios_recreativos_otro", "discapacidad", "adaptabilidad_comida",
+                 "admision_mascotas", "n_turistas_alta", "gasto_turista", "ingreso_otra",
+                 "actividad_complementaria", "actividad_complementaria_otra", "n_personas_emprendimiento_alta",
+                 "n_personas_emprendimiento_baja", "n_personas_emprendimiento_familia", "financiamiento",
+                 "financiamiento_especifique", "financiamiento_institucion", "financiamiento_institucion_especifique",
+                 "invita_actividad", "invita_actividad_otra", "compra_comunidad", "relaciones_externas",
+                 "vinculacion_otros_actores", "artesania_local", "resguardo_patrimonial", "informacion_ancestral",
+                 "normas_comportamiento_personas", "normas_comportamiento_servicios", "gestion_basura",
+                 "gestion_basura_otro", "eficiencia_energetica", "eficiencia_energetica_cual",
+                 "disponibilidad_agua", "abastecimiento_agua", "recuperacion_ambiental", "capacidad_de_carga",
+                 "turismo_afectado", "capacitaciones_recibidas", "capacitaciones_necesitadas", "venta_total",
+                 "crisis_desarrollo", "crisis_tipo_evento", "crisis_tipo_evento_otro", "crisis_desercion_laboral",
+                 "crisis_recuperacion", "crisis_medios_comunicacion", "crisis_medios_comunicacion_otros",
+                 "crisis_actores", "crisis_actores_cual", "crisis_protocolo_emergencia", "crisis_zona_riesgo",
+                 "crisis_zona_segura", "crisis_protocolo_sanitario", "crisis_primeros_auxilios")
 
-
-
-
-
+# Verificar los nuevos nombres de las variables
+names(bbdd)
 
 
